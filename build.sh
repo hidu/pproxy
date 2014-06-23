2881a0f093b1d1de9206ec9e685385265fff6785
duwei <duv123@gmail.com>
2014-06-23 23:07:38 +0800

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+# build for window: build.sh windows
+# default linux
+#./gox -build-toolchain
 
 set -e
 cd $(dirname $0)
 
 dest_file="pproxy"
+
+DEST_OS=$1
+if [ "$DEST_OS" == "windows" ];then
+  export GOOS=windows 
+  export GOARCH=386
+  dest_file="pproxy.exe"
+fi
 
 go build -o $dest_file -ldflags "-s -w"  proxy_main.go 
 zip -r res.zip res
